c211050829e334c3ff607ebb62c9731515c8f128
Nana <160034837+a-aaangeeel@users.noreply.github.com>
2024-03-23 19:42:34 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,32 +6,24 @@ int  left_index = 0;
 int right_index = size - 1;
 int i = 0;
 int middle = 0;
- while (left_index <= right_index) 
-  {
+ while (left_index <= right_index) {
     int middle = left_index + (right_index - left_index) / 2;
-    if (arr[middle] == value) 
-     { 
+    if (arr[middle] == value) {
         count++;
-        for ( i = middle - 1; i >= 0 && arr[i] == value; --i) 
-	{
-	count++;
-	}
-	for ( i = middle + 1; i < size && arr[i] == value; ++i) 
-        {
-	 count++;
-        }
+        for ( i = middle - 1; i >= 0 && arr[i] == value; --i) {
+          count++;
+         }
+         for ( i = middle + 1; i < size && arr[i] == value; ++i) {
+         count++;
+         }
          return count;
-     }
-     else if (arr[middle] < value) 
-      {
-              left_index = middle + 1;
+         }
+         else if (arr[middle] < value) {
+          left_index = middle + 1;
       }
-      else 
-      {
+      else {
         right_index = middle - 1;
       }
-   }
-   return count;
- } 
-
-
+ }
+     return count;
+ }
